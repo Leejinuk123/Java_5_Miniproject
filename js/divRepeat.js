b0a7b6a6f4a792abfe2a318f0775f4712636c630
leejinuk <leejinouk123@kakao.com>
2024-04-05 17:00:20 +0900

    모든 코드 리팩토링

--- js/divRepeat.js
+++ js/divRepeat.js
@@ -1,21 +1,25 @@
 // 각 .card 요소에 이미지와 이름, 소개 추가하는 모듈
 let createCardImg = document.querySelectorAll('.card');
 const cardData = [
-  {"name":"김민규",
-    "intro":"자기소개",
-    "img":"https://3.bp.blogspot.com/-fRPuZTBN2rE/TodGV8UaVHI/AAAAAAAAO_o/G2xJUAZuawQ/s1600/pingu.jpg"
+  {
+    "name": "김민규",
+    "intro": "자기소개",
+    "img": "https://3.bp.blogspot.com/-fRPuZTBN2rE/TodGV8UaVHI/AAAAAAAAO_o/G2xJUAZuawQ/s1600/pingu.jpg"
   },
-  {"name":"이진욱",
-    "intro":"자기소개",
-    "img":"https://3.bp.blogspot.com/-fRPuZTBN2rE/TodGV8UaVHI/AAAAAAAAO_o/G2xJUAZuawQ/s1600/pingu.jpg"
+  {
+    "name": "이진욱",
+    "intro": "자기소개",
+    "img": "https://3.bp.blogspot.com/-fRPuZTBN2rE/TodGV8UaVHI/AAAAAAAAO_o/G2xJUAZuawQ/s1600/pingu.jpg"
   },
-  {"name":"주장현",
-    "intro":"하루에 국밥 네끼<br> 먹는거 좋아하는 소개",
-    "img":"https://i.ytimg.com/vi/euMU4VK8cQ4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBNWv0UceagUgZa5WpP419uMVUf6A"
+  {
+    "name": "주장현",
+    "intro": "하루에 국밥 네끼<br> 먹는거 좋아하는 소개",
+    "img": "https://i.ytimg.com/vi/euMU4VK8cQ4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBNWv0UceagUgZa5WpP419uMVUf6A"
   },
-  {"name":"원지연",
-    "intro":"자기소개",
-    "img":"https://3.bp.blogspot.com/-fRPuZTBN2rE/TodGV8UaVHI/AAAAAAAAO_o/G2xJUAZuawQ/s1600/pingu.jpg"
+  {
+    "name": "원지연",
+    "intro": "자기소개",
+    "img": "https://3.bp.blogspot.com/-fRPuZTBN2rE/TodGV8UaVHI/AAAAAAAAO_o/G2xJUAZuawQ/s1600/pingu.jpg"
   }
 ];
 
